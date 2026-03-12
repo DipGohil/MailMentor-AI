c5243465423f51eef3fd82bc71e1c64505ee11b6
Dip Gohil <gohildeep001@gmail.com>
2026-03-12 11:30:15 +0530

    Updated dashboard to display list of important emails

--- dashboard/app.py
+++ dashboard/app.py
@@ -56,22 +56,6 @@ priority_emails = [
     if e.get("priority") == "Important"
 ][:5] # Limits to show only 5
 
-# if priority_emails:
-
-#     st.markdown("## 🚨 Important Emails")
-
-#     for email in priority_emails:
-
-#         st.markdown(
-#             f"""
-#             <div style="border-left:6px solid red;padding:12px;background:#2a1a1a;border-radius:6px;margin-bottom:10px">
-#             <b>{email['subject']}</b><br>
-#             <small>From: {email['sender']}</small>
-#             </div>
-#             """,
-#             unsafe_allow_html=True
-#         )
-
 if priority_emails:
 
     st.markdown("## 🚨 Important Emails")
@@ -80,46 +64,14 @@ if priority_emails:
 
         for email in priority_emails:
 
-            col1, col2 = st.columns([4,1])
-
-            with col1:
-                st.markdown(
+            st.markdown(
                     f"""
                     **{email['subject']}**  
                     <small>From: {email['sender']}</small>
                     """,
                     unsafe_allow_html=True
                 )
-
-            with col2:
-                if st.button("Open", key=email["id"]):
-                    st.session_state["opened_email_id"] = email["id"]
-                    
-            if st.session_state.get("opened_email_id") == email["id"]:
-
-                res = requests.get(
-                    f"{API_URL}/analytics/email/{email['id']}"
-                )
-
-                if res.status_code == 200:
-
-                    data = res.json()
-
-                    st.markdown(
-                        f"""
-                        <div style="background:#111827;padding:15px;border-radius:8px;margin-top:10px">
-
-                        <b>Subject:</b> {data['subject']}<br>
-                        <b>From:</b> {data['sender']}<br><br>
-
-                        <b>Body:</b><br>
-                        {data['body']}
-
-                        </div>
-                        """,
-                        unsafe_allow_html=True
-                    )
-           
+                      
             st.divider()
 
 
